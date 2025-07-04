9ccc8997122db4749955623be5cf07c0bd12048b
Jasom Rocha <36940932+JasomRocha@users.noreply.github.com>
2025-07-04 01:02:13 -0300

    Adc rota dummy de login para evitar RoutNotFoundExeption

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return response()->json(['message' => 'Login via API apenas.'], 404);
+})->name('login');
